9ac976f6d07861cb3aa1a0f018f50faf90a30755
Rita Rangel <rita.d.rangel@accenture.com>
2023-09-23 14:10:12 -0300

    feature: order menu without integrations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { ProductsMenuComponent } from './components/products-menu/products-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    ProductsMenuComponent
   ],
   imports: [
     BrowserModule,
