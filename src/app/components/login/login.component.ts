dcf07f676d2dc2fe6defc0c030c31b40d6dbfe09
Rita Rangel <rita.d.rangel@accenture.com>
2023-09-24 15:16:05 -0300

    feature: login-screen and media adjustments (mobile first)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+
+export class LoginComponent implements OnInit {
+
+  login = false;
+  cadastro = true;
+  esqueciSenha = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
