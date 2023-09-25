1e69036419637b453deeb14801548eb037af6484
Rita Rangel <rita.d.rangel@accenture.com>
2023-09-24 22:57:26 -0300

    feature: landing-page and header navigation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
 
+export class HeaderComponent implements OnInit {
+
+  logged = false;
+  @Input() currentPage = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.checkCurrentPage();
+    this.checkLogin();
+  }
+
+  checkLogin(){
+    if (this.logged === false){
+      let collection = document.getElementsByClassName('filled-button-iconBG');
+      Array.from(collection).forEach(element => {
+        element.classList.add('inactive');
+      });
+    }else if(this.logged === true){
+      let collection = document.getElementsByClassName('filled-button-iconBG');
+      Array.from(collection).forEach(element => {
+        element.classList.add('active');
+      });
+    }
+  }
+
+  checkCurrentPage() {
+    if (this.currentPage === 'landing-page') {
+      document.getElementById('inicio')?.classList.add('active');
+      document.getElementById('cardapio')?.classList.remove('active');
+      document.getElementById('acessar')?.classList.remove('active');
+    } else if (this.currentPage === 'cardapio') {
+      document.getElementById('inicio')?.classList.remove('active');
+      document.getElementById('cardapio')?.classList.add('active');
+      document.getElementById('acessar')?.classList.remove('active');
+    } else if (this.currentPage === 'acessar') {
+      document.getElementById('inicio')?.classList.remove('active');
+      document.getElementById('cardapio')?.classList.remove('active');
+      document.getElementById('acessar')?.classList.add('active');
+    }
+  }
 }
--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent {
 
+export class LandingPageComponent implements OnInit {
+
+  logged = false;
+  @Input() currentPage = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.checkLogin();
+  }
+
+  checkLogin(){
+    if (this.logged === false){
+      document.getElementById('acessar')?.classList.remove('hidden');
+    }else if(this.logged === true){
+      document.getElementById('acessar')?.classList.add('hidden');
+    }
+  }
 }
