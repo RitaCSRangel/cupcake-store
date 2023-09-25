d8f11e2584933a8f10e9c1953ee207dd74e0b2f4
Rita Rangel <rita.d.rangel@accenture.com>
2023-09-24 21:52:59 -0300

    feature: last orders

--- src/app/components/last-orders/last-orders.component.ts
+++ src/app/components/last-orders/last-orders.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-last-orders',
   templateUrl: './last-orders.component.html',
   styleUrls: ['./last-orders.component.scss']
 })
-export class LastOrdersComponent {
+
+export class LastOrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
