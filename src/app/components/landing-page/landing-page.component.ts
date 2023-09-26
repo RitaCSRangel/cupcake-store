21398c1dfa812dbee68338b6a2911f780f73f991
Rita Rangel <rita.d.rangel@accenture.com>
2023-09-25 22:31:47 -0300

    feature: landing-page navigation without cart

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -8,20 +8,61 @@ import { Component, Input, OnInit } from '@angular/core';
 
 export class LandingPageComponent implements OnInit {
 
+  // Controlers
   logged = false;
   @Input() currentPage = '';
+
+  // Page data
+  popularItems: any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    sessionStorage.setItem('user', 'Rita');
     this.checkLogin();
+    this.loadPopularItems();
   }
 
   checkLogin(){
+
+    try{
+      const loggedUser = sessionStorage.getItem('user');
+      if (loggedUser){
+        document.getElementById('acessar')?.classList.add('hidden');
+        this.logged = true;
+      }else{
+        document.getElementById('acessar')?.classList.remove('hidden');
+        this.logged = false;
+      }
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+  loadPopularItems(){
+    var item = {
+      name: 'Vanilla Latte',
+      value: 20,
+      quantity: '10',
+      score: '5',
+      image: '../../../assets/landing-page/product-image-example.png'
+    }
+
+    this.popularItems.push(item);
+    this.popularItems.push(item);
+    this.popularItems.push(item);
+    this.popularItems.push(item);
+  }
+
+  addToCart(index: number){
     if (this.logged === false){
-      document.getElementById('acessar')?.classList.remove('hidden');
-    }else if(this.logged === true){
-      document.getElementById('acessar')?.classList.add('hidden');
+      if (document.getElementById(`cardalert-${index}`)?.classList.contains('invisible')){
+        document.getElementById(`cardalert-${index}`)?.classList.remove('invisible');
+        document.getElementById(`cardalert-${index}`)?.classList.add('visible');
+      }else{
+        document.getElementById(`cardalert-${index}`)?.classList.add('invisible');
+        document.getElementById(`cardalert-${index}`)?.classList.remove('visible');
+      }
     }
   }
 }
